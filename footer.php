802fa7771618cf199a4ff9f5d6fd264f4626cbd6
Yunyi1 <yunyi.xie@outlook.com>
2020-02-21 09:46:01 +0100

    update

---
+++ footer.php
@@ -0,0 +1,58 @@
+  <!-- FOOTER START -->
+    <footer class="footer-main">
+      <footer class="footer-container">
+
+        <footer class="footer-left">
+
+          <footer class="links">
+            <a href="index.php">Home</a>
+            <a href="mailto:skoek@mborijnland.nl" target="_top">Contact</a>
+            <a href="https://mborijnland.nl/cookies/" target="_blank"
+              >Cookies</a
+            >
+            <a href="https://mborijnland.nl/disclaimer/" target="_blank"
+              >Disclaimer</a
+            >
+            <a class="none-line"href="https://mborijnland.nl/privacyverklaring/" target="_blank"
+              >Privacy</a
+            >
+          </footer>
+
+          <footer class="copyright">
+            <p>All Rights Reserved 2020 © Bakkerij Leiden</p>
+          </footer>
+
+        </footer>
+        <!-- End of Footer left -->
+
+        <footer class="footer-right">
+
+          <footer class="social">
+            <a href="https://www.facebook.com/mboRijnland" target="_blank"
+              ><i class="fab fa-facebook-square"></i> Facebook</a
+            >
+            <a href="https://www.instagram.com/mborijnland/" target="_blank"
+              ><i class="fab fa-instagram"></i> Instagram</a
+            >
+            <a
+              href="https://www.youtube.com/channel/UCFmiIyIrkm03gPDR3TcWKBQ"
+              target="_blank"
+              ><i class="fab fa-youtube"></i> YouTube</a
+            >
+            <a href="https://twitter.com/mborijnland" target="_blank"
+              ><i class="fab fa-twitter"></i> Twitter</a
+            >
+            <a
+              href="https://www.linkedin.com/school/mborijnland/"
+              target="_blank"
+              ><i class="fab fa-linkedin"></i> LinkedIn</a
+            >
+          </footer>
+
+        </footer>
+        <!-- End of Footer right -->
+      </footer>
+    </footer>
+    <!-- FOOTER END -->
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+ include "header.php";
+?>
+
+<!-- MAIN CONTENT HERE -->
+
+<div id="container">
+  <div id="block">
+  
+  </div>
+  <div class="box">
+
+  <h2>Verassingpakket 1</h2>
+
+  </div>
+
+  <div class="box">
+
+  <h2>Verassingpakket 2</h2>
+
+  </div>
+
+  <div class="none">
+ 
+  <h2>Verassingpakket 3</h2>
+ 
+  </div>
+</div>
+
+
+<?php
+  include "footer.php";
+?>
