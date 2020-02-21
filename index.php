14ff7c6c4457ef2df9479ac8b1b5a7f5e68adb3a
DaniqueGIT <57901116+DaniqueGIT@users.noreply.github.com>
2020-02-21 13:32:37 +0100

    123

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bakkerij_leiden";
+// $products = "SELECT * FROM `products`";
+
+try {
+    $db = new PDO("mysql:host=$servername;dbname={$dbname}", $username, $password);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
+<?php
  include "header.php";
 ?>
 
@@ -6,24 +24,24 @@
 
 <div id="container">
   <div id="block">
-  
+
   </div>
   <div class="box">
 
-  <h2>Verassingpakket 1</h2>
-
+    <h2>Verassingpakket 1</h2>
+    
   </div>
 
   <div class="box">
 
-  <h2>Verassingpakket 2</h2>
+    <h2>Verassingpakket 2</h2>
 
   </div>
 
   <div class="none">
- 
-  <h2>Verassingpakket 3</h2>
- 
+
+    <h2>Verassingpakket 3</h2>
+
   </div>
 </div>
 
